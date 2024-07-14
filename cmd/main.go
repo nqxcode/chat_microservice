a3b78ae0b63bbbe8ebcef4251a292b8c0f9ff4b1
Andrey Pokoev <avpokoyev@mts.ru>
2024-07-14 23:17:54 +0300

    week_1 Added chat proto file, generate chat grpc server.

--- cmd/main.go
+++ cmd/main.go
@@ -3,8 +3,16 @@ package main
 import (
 	"fmt"
 
+	//desc "github.com/nqxcode/chat_microservice/pkg/chat_v1"
+
 	"github.com/fatih/color"
 )
+
+const grpcPort = 50052
+
+//type server struct {
+//	desc.UnimplementedAuthV1Server
+//}
 
 func main() {
 	fmt.Println(color.GreenString("Hello, world!"))
