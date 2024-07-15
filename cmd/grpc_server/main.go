cc1f9f4350a8f553ef55b7fc84a604b86e8bfbe8
Andrey Pokoev <avpokoyev@mts.ru>
2024-07-15 10:10:50 +0300

    week_1 Fix after review.

--- cmd/grpc_server/main.go
+++ cmd/grpc_server/main.go
@@ -20,6 +20,23 @@ type server struct {
 	desc.UnimplementedChatV1Server
 }
 
+func main() {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	desc.RegisterChatV1Server(s, &server{})
+
+	log.Printf("server listening at %v", lis.Addr())
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
+
 func (s *server) Create(_ context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
 	log.Printf("User names: %v", req.GetUsernames())
 
@@ -38,21 +55,4 @@ func (s *server) SendMessage(_ context.Context, req *desc.SendMessageRequest) (*
 	log.Printf("Send message: %v from %v at %v", req.GetMessage(), req.GetFrom(), req.GetTimestamp())
 
 	return nil, nil
-}
-
-func main() {
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
-
-	s := grpc.NewServer()
-	reflection.Register(s)
-	desc.RegisterChatV1Server(s, &server{})
-
-	log.Printf("server listening at %v", lis.Addr())
-
-	if err = s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
-	}
 }
