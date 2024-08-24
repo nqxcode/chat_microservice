9cfd3c01a88125d37382dbd24af36aec64e5076a
Andrey Pokoev <avpokoyev@mts.ru>
2024-08-24 14:01:02 +0300

    week_4 WIP

--- internal/service/service.go
+++ internal/service/service.go
@@ -10,7 +10,7 @@ import (
 type ChatService interface {
 	Create(ctx context.Context, chat *model.ChatInfo) (int64, error)
 	Delete(ctx context.Context, id int64) error
-	Find(ctx context.Context, id int64) (*model.Chat, error)
+	Get(ctx context.Context, id int64) (*model.Chat, error)
 	SendMessage(ctx context.Context, message *model.Message) (int64, error)
 	GetMessages(ctx context.Context, id int64, limit *pagination.Limit) ([]model.Message, error)
 }
