6c52f03f7b82fcdbb550f3ddb44ae0951c4b65a2
Andrey Pokoev <avpokoyev@mts.ru>
2024-09-22 00:43:31 +0300

    week_6 Added graceful shutdown.

--- internal/app/app.go
+++ internal/app/app.go
@@ -5,6 +5,10 @@ import (
 	"flag"
 	"log"
 	"net"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 
 	"github.com/nqxcode/platform_common/closer"
 	"google.golang.org/grpc"
@@ -40,13 +44,29 @@ func NewApp(ctx context.Context) (*App, error) {
 }
 
 // Run run application
-func (a *App) Run() error {
+func (a *App) Run(ctx context.Context) error {
 	defer func() {
 		closer.CloseAll()
 		closer.Wait()
 	}()
 
-	return a.runGRPCServer()
+	ctx, cancel := context.WithCancel(ctx)
+
+	wg := &sync.WaitGroup{}
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		err := a.runGRPCServer(ctx)
+		if err != nil {
+			log.Printf("failed to run GRPC server: %v", err)
+		}
+	}()
+
+	gracefulShutdown(ctx, cancel, wg)
+
+	return nil
 }
 
 func (a *App) initDeps(ctx context.Context) error {
@@ -90,8 +110,14 @@ func (a *App) initGRPCServer(ctx context.Context) error {
 	return nil
 }
 
-func (a *App) runGRPCServer() error {
+func (a *App) runGRPCServer(ctx context.Context) error {
 	log.Printf("GRPC server is running on %s", a.serviceProvider.GRPCConfig().Address())
+
+	go func() {
+		<-ctx.Done()
+		a.grpcServer.GracefulStop()
+		log.Printf("GRPC server gracefully stopped")
+	}()
 
 	listener, err := net.Listen("tcp", a.serviceProvider.GRPCConfig().Address())
 	if err != nil {
@@ -104,4 +130,24 @@ func (a *App) runGRPCServer() error {
 	}
 
 	return nil
+}
+
+func gracefulShutdown(ctx context.Context, cancel context.CancelFunc, wg *sync.WaitGroup) {
+	select {
+	case <-ctx.Done():
+		log.Println("terminating: context cancelled")
+	case <-waitSignal():
+		log.Println("terminating: via signal")
+	}
+
+	cancel()
+	if wg != nil {
+		wg.Wait()
+	}
+}
+
+func waitSignal() chan os.Signal {
+	sigterm := make(chan os.Signal, 1)
+	signal.Notify(sigterm, syscall.SIGINT, syscall.SIGTERM)
+	return sigterm
 }
